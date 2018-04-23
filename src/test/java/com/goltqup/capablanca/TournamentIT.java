f9281f094fd391a876b5bbf1daa48721c6638ded
Jonatan_Mendoza <yonatanimm@gmail.com>
2018-04-23 16:01:26 -0600

    Renames test

--- src/test/java/com/goltqup/capablanca/TournamentIT.java
+++ src/test/java/com/goltqup/capablanca/TournamentIT.java
@@ -19,7 +19,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 @SpringBootTest
 @AutoConfigureWebTestClient
 @AutoConfigureWebFlux
-public class TournamentControllerIT {
+public class TournamentIT {
 
     @Autowired
     private TournamentIdEncoder tournamentIdEncoder;
